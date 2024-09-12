0598c01537093dc4e3704fb83d620a73ba0b2eb6
Artur032 <karlinartur30@gmail.com>
2024-09-12 15:31:35 +0200

    12.09.24

--- Vertex.java
+++ Vertex.java
@@ -1,6 +1,7 @@
 class Vertex {
     public double x;
     public double y;
+    
 
 public Vertex(double x,double y){
  this.x=x;
@@ -11,13 +12,29 @@ public Vertex(double x,double y){
     public static void main(String[] args) {
         Vertex v1= new Vertex(42, 17);
         Vertex v2= new Vertex(4, 7.2);
-        System.out.println(v1);
-        v1.x=v1.x + v2.x;
-        System.out.println(v1);
+        Vertex v3= v1.skalarMult(2);
+        System.out.println(v1.length());
+        System.out.println(v2.length());
+   
+    }
+    public Vertex skalarMult(double s){
+        return new Vertex(x*s,y*s);
+    }
+    public void skalarMultMod(double s){
+        x = x*s;
+        y = y*s;
     }
     public String toString() {
         return "("+x+","+y+")";
-
+       
+    }
+    public double length(){
+        return Math.sqrt(x*x+y*y);
     }
 }
 
+
+    
+
+
+   
