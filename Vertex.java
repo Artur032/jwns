8c6cdee02995e6c016d91ea21f3498dd9cb78692
Artur032 <karlinartur30@gmail.com>
2024-09-17 08:31:43 +0200

    weitere funktion

--- Vertex.java
+++ Vertex.java
@@ -1,41 +1,70 @@
 class Vertex {
     public double x;
     public double y;
-    
 
-public Vertex(double x,double y){
- this.x=x;
- this.y=y;
- 
-    
+    public Vertex(double x, double y) {
+        this.x = x;
+        this.y = y;
 
     }
+
     public static void main(String[] args) {
-        Vertex v1= new Vertex(42, 17);
-        Vertex v2= new Vertex(4, 7.2);
-        Vertex v3= v1.skalarMult(2);
+        Vertex v1 = new Vertex(42, 17);
+        Vertex v2 = new Vertex(4, 7.2);
+        Vertex v3 = v1.skalarMult(2);
         System.out.println(v1.length());
         System.out.println(v2.length());
-   
+
     }
-    public Vertex skalarMult(double s){
-        return new Vertex(x*s,y*s);
+
+    public double length() {
+        return Math.sqrt(x * x + y * y);
     }
-    public void skalarMultMod(double s){
-        x = x*s;
-        y = y*s;
+
+    public Vertex skalarMult(double s) {
+        return new Vertex(x * s, y * s);
     }
+
+    public void skalarMultMod(double s) {
+        x = x * s;
+        y = y * s;
+    }
+
     public String toString() {
-        return "("+x+","+y+")";
-       
+        return "(" + x + "," + y + ")";
+
     }
-    public double length(){
-        return Math.sqrt(x*x+y*y);
+
+    public Vertex add(Vertex v2) {
+        return new Vertex(x + v2.x, y + v2.y);
     }
+
+    public void addMod(Vertex v2) {
+        x += v2.x;
+        y += v2.y;
+    }
+
+    public double setX(double x) {
+        this.x = x;
+    }
+
+    public double setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+public boolean equals(Object thatObject){
+    if (that instanceof Object){
+        that Vertex = (Vertex) thatObject;
+        return this.x==thatObject.x && this.y==thatObject.y;
+    }
+    return false;
 }
-
-
-    
-
-
-   
+}
